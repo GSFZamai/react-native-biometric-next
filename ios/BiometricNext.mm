82c310dbdf82b263fd9827513be48ce590de3967
Felipe Paschoal <felipe.paschoal@bkbank.com.br>
2024-12-26 11:16:42 -0300

    feat(iOS): implements checkNewFingerPrintAdded method on iOS and removes mutiply method

--- ios/BiometricNext.mm
+++ ios/BiometricNext.mm
@@ -2,11 +2,9 @@
 
 @interface RCT_EXTERN_MODULE(BiometricNext, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-
 RCT_EXTERN_METHOD(enableBioMetric:(NSString *)title subtitle:(NSString *)subtitle callback:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(checkNewFingerPrintAdded:(RCTResponseSenderBlock)callback)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/BiometricNext.swift
+++ ios/BiometricNext.swift
@@ -1,15 +1,16 @@
 import LocalAuthentication
+
+let biometricKey = "BiometricsPolicyState"
+
 @objc(BiometricNext)
 class BiometricNext: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    
     
     @objc func enableBioMetric(_ title: String,subtitle: String, callback: @escaping RCTResponseSenderBlock) -> Void {
         let localAuthenticationContext = LAContext()
         localAuthenticationContext.localizedFallbackTitle = title
+        
         var authError: NSError?
         let reasonString = subtitle
         if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError) {
@@ -42,5 +43,40 @@ class BiometricNext: NSObject {
             }
             callback([authError ?? "Something went wrong"])
         }
+    }
+    
+    @objc func checkNewFingerPrintAdded(_ callback: @escaping RCTResponseSenderBlock) {
+        callback([LAContext.biometricsChanged()])
+    }
+}
+
+
+extension LAContext {
+    
+    static var savedBiometricPolicyState: Data? {
+        get {
+            UserDefaults.standard.data(forKey: biometricKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: biometricKey)
+        }
+    }
+    
+    static func biometricsChanged() -> Bool {
+        let context = LAContext()
+        var error: NSError?
+        
+        context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+        
+        if error == nil && LAContext.savedBiometricPolicyState == nil {
+            LAContext.savedBiometricPolicyState = context.evaluatedPolicyDomainState
+            return false
+        }
+        
+        if let domainState = context.evaluatedPolicyDomainState, domainState != LAContext.savedBiometricPolicyState {
+            return true
+        }
+        
+        return false
     }
 }
